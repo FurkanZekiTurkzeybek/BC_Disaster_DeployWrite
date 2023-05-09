efde1cd244527e003e4fdbd7470abcfca6780275
FurkanZekiTurkzeybek <furkan.fztr@gmail.com>
2023-05-10 02:20:20 +0300

    -Server has been set.

--- deploy.js
+++ deploy.js
@@ -15,7 +15,7 @@ let userRef = db.collection("Users");
 //ethereum init starts
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require("web3");
-const{interface, bytecode} = require("./compile");
+const {interface, bytecode} = require("./compile");
 
 const provider = new HDWalletProvider(
     'evoke abuse cigar skate stay mercy avocado dilemma mass token spider napkin',
@@ -24,37 +24,59 @@ const provider = new HDWalletProvider(
 
 const web3 = new Web3(provider);
 //ethereum init ends.
+const express = require('express');
+const { Server } = require('ws');
+const PORT = process.env.PORT || 3000;
 
+const server = express()
+    .use((req, res) => res.send("Hi there"))
+    .listen(PORT, () => console.log('Listening on port ${PORT}'));
+const wss = new Server({ server });
 
+wss.on('connection', async function (ws, req) {
+    await ws.on('message', async message => {
+        var dataString = message.toString();
+        console.log(dataString)
+        const input = dataString.split(" ");
+        if (dataString != null) {
+            await deploy(input[0], input[1], input[2], input[3]);
+            // console.log("Input is recieved");
 
-let name = 'def name';
-let surnmae = 'def surname';
-let SSN = 'def SSN';
-let address = 'def address';
+        } else {
+            console.log("data string is null");
+        }
+    })
+})
 
-const deploy = async ()=> {
+// let name = 'def name';
+// let surnmae = 'def surname';
+// let SSN = 'def SSN';
+// let address = 'def address';
+
+const deploy = async (pName, pSurname, pSSN, pAddress) => {
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account ',accounts[0]);
+    console.log('Attempting to deploy from account ', accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode, arguments: [name,surnmae,SSN,address]})
-    .send({gas: '1000000', from: accounts[0]});
+        .deploy({data: bytecode, arguments: [pName, pSurname, pSSN, pAddress]})
+        .send({gas: '1000000', from: accounts[0]});
 
-    console.log('Contract deployed to',result.options.address);
+    console.log('Contract deployed to', result.options.address);
     const hashCode = result.options.address;
     const data = {
-    id: 2,
-    name : name,
-    surname : surnmae,
-    SSN : SSN,
-    address : address,
-    hash : hashCode
-}
+        id: 2,
+        name: pName,
+        surname: pSurname,
+        SSN: pSSN,
+        address: pAddress,
+        hash: hashCode
+    }
 
     db.collection("Users").add(data);
     provider.engine.stop();
 };
+
 //This function is for testing
 function write() {
     const contract = new web3.eth.Contract(JSON.parse(interface),
@@ -65,4 +87,4 @@ function write() {
             console.log(receipt);
         });
 }
-deploy();
+
